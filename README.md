eaff8dd9d56b575f45f6565330b5cb36ce5a41d0
FabioTV <fabiothomaz.97@gmail.com>
2022-01-27 15:22:33 -0300

    [feature] TD001 - full sample ToDos API

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
 # ToDos
-A simple API that will enable you to craate a list of todos
+A simple API that will enable you to craate a list of todos<br>
+<br>
+## Requiriments<br>
+&ensp;[x] Shoud be able to create a new user<br>
+&ensp;[x] Shoud be able to list all user's todos<br>
+&ensp;[x] Shoud be able to create a todo<br>
+&ensp;[x] Shoud be able to update a todo<br>
+&ensp;[x] Shoud be able to update a todo<br>
+&ensp;[x] Shoud be able to mark a todo as done<br>
+&ensp;[x] Shoud be able to delete a todo<br>
+<br>
+## Business rules<br>
+&ensp;[x] Shoudn't be able to create a new user when the username already exists<br>
+&ensp;[x] Shoudn't be able to list all user's todos when the username is invalid or don't exists<br>
+&ensp;[x] Shoudn't be able to update a todo if the todo id is invalid or don't exists<br>
+&ensp;[x] Shoudn't be able to markt a todo as done if the todo id is invalid or don't exists<br>
+&ensp;[x] Shoudn't be able to delete a todo if the todo id is invalid or don't exists<br>
---
+++ src/index.js
@@ -0,0 +1,106 @@
+const express = require('express');
+const cors = require('cors');
+
+const { v4: uuidv4 } = require('uuid');
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+const users = [];
+
+function checksExistsUserAccount(request, response, next) {
+  const { username } = request.headers;
+
+  const user = users.find((user) => user.username === username);
+
+  if (!user) return response.status(404).json({error: "User not found!"});
+
+  request.user = user;
+
+  return next();
+}
+
+app.post('/users', (request, response) => {
+  const { name, username } = request.body;
+
+  const userAlreadyExist = users.some((user) => user.username === username);
+
+  if (userAlreadyExist) return response.status(400).json({error: "User already exist!"});
+
+  const userStructure = {
+    id: uuidv4(),
+    name,
+    username,
+    todos: []
+  }
+
+  users.push(userStructure);
+  response.status(201).json(userStructure);
+});
+
+app.get('/todos', checksExistsUserAccount, (request, response) => {
+  const { user } = request;
+  response.json(user.todos);
+});
+
+app.post('/todos', checksExistsUserAccount, (request, response) => {
+  const { title, deadline } = request.body;
+  const { user } = request;
+
+  const todo = {
+    id: uuidv4(),
+    title,
+    done: false,
+    deadline: new Date(deadline),
+    created_at: new Date()
+  }
+
+  user.todos.push(todo);
+
+  return response.status(201).json(todo);
+});
+
+app.put('/todos/:id', checksExistsUserAccount, (request, response) => {
+  const { id } = request.params;
+  const { title, deadline } = request.body;
+  const { user } = request;
+
+  const todo = user.todos.find((todo) => todo.id === id);
+
+  if (!todo) return response.status(400).json({error: "todo don't exist!"});
+
+  todo.title = title;
+  todo.deadline = new Date(deadline);
+
+  return response.status(201).send();
+});
+
+app.patch('/todos/:id/done', checksExistsUserAccount, (request, response) => {
+  const { id } = request.params;
+  const { user } = request;
+
+  const todo = user.todos.find((todo) => todo.id === id);
+
+  if (!todo) return response.status(400).json({error: "todo don't exist!"});
+
+  todo.done = true;
+
+  return response.status(201).send();
+});
+
+app.delete('/todos/:id', checksExistsUserAccount, (request, response) => {
+  const { id } = request.params;
+  const { user } = request;
+
+  const todo = user.todos.find((todo) => (todo.id === id));
+
+  if (!todo) return response.status(400).json({error: "todo don't exist!"});
+
+  user.todos.splice(todo, 1);
+
+  return response.status(200).json(user);
+});
+
+module.exports = app;
